ff678e9e37bd88c4d3e52faa3328dd172970cbd7
ashwinms705-crypto <ashwinms705@gmail.com>
2026-06-25 14:46:48 +0530

    Time: 35 ms (43.63%), Space: 60.1 MB (15.6%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        List<List<Integer>> res=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    List<Integer> l=new ArrayList<>();
+                    l.add(nums[i]);l.add(nums[j]);l.add(nums[k]);
+                    res.add(l);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+}
