76be790dab52b329f8a9ba652b823b4184ea93ca
ashwinms705-crypto <ashwinms705@gmail.com>
2026-07-08 18:47:51 +0530

    Time: 7 ms (41.85%), Space: 46.7 MB (21.79%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,19 +1,20 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) ->
-            a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]
-        );
-
-        int count = 0;
-        int maxEnd = 0;
-
-        for (int[] interval : intervals) {
-            if (interval[1] > maxEnd) {
-                count++;
-                maxEnd = interval[1];
+        Arrays.sort(intervals,(a,b)->{
+            if(a[0]==b[0])
+                return Integer.compare(b[1],a[1]);
+            return Integer.compare(a[0],b[0]);
+        });
+        int prev[]=intervals[0];
+        int c=intervals.length;
+        for(int i=1;i<intervals.length;i++){
+            if(prev[0]<=intervals[i][0] && intervals[i][1]<=prev[1]){
+                c--;
+            }
+            else{
+                prev=intervals[i];
             }
         }
-
-        return count;
+        return c;
     }
 }
