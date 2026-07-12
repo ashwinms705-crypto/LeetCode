0e74eadfa46b8c8872f1f36678865ee241f457a3
ashwinms705-crypto <ashwinms705@gmail.com>
2026-07-13 00:13:34 +0530

    Time: 28 ms (93.14%), Space: 75.4 MB (57.67%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        TreeMap<Integer,List<Integer>> tm=new TreeMap<>();
+        int res[]=new int[arr.length];
+        /*TreeMap<Integer,List<Integer>> tm=new TreeMap<>();
         for(int i=0;i<arr.length;i++){
             if(tm.containsKey(arr[i])){
                 tm.get(arr[i]).add(i);
@@ -18,6 +19,24 @@ class Solution {
                 res[k]=r;
             }
             r++;
+        }*/
+        int temp[]=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            temp[i]=arr[i];
+        }
+        Arrays.sort(temp);
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        int r=1;
+        for(int i=0;i<temp.length;i++){
+            if(!hm.containsKey(temp[i])){
+                hm.put(temp[i],r++);
+            }
+            else{
+                continue;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            res[i]=hm.get(arr[i]);
         }
         return res;
     }
