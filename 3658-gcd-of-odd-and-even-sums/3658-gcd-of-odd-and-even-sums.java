21fae97ecbca7f39151af73c0bfb9051d5349da9
ashwinms705-crypto <ashwinms705@gmail.com>
2026-07-15 08:05:24 +0530

    Time: 1 ms (83.11%), Space: 42.5 MB (57.16%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,20 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        /*int sume=0;
-        int sumo=0;
-        for(int i=1;i<=n*2;i++){
-            if(i%2==0)
-                sume+=i;
-            else
-                sumo+=i;
-        }
-        int gcd=1;
-        for(int i=2;i<=sume;i++){
-            if(sumo%i==0 && sume%i==0){
-                gcd=Math.max(gcd,i);
-            }
-        }
-        //return gcd;*/
         return n;
     }
 }
