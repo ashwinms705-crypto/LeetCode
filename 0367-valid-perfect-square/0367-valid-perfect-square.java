aeee3517a5569df7e30c1e9440809a17fef5b0ab
ashwinms705-crypto <ashwinms705@gmail.com>
2026-09-17 07:43:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (87.47%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int l=1;
+        int r=num;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(((long)mid*mid)==num) return true;
+            else if(((long)mid*mid)>num)
+                r=mid-1;
+            else
+                l=mid+1;
+
+        }
+        return false;
+    }
+}
